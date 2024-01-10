3e2ec600133037562d538970bfcac1c790d7a844
Oscar Hill <OscarPi@users.noreply.github.com>
2024-01-10 13:37:21 +0000

    Change pinout in usb_microphone example to match Arduino Nano RP2040 Connect built in microphone

--- examples/usb_microphone/main.c
+++ examples/usb_microphone/main.c
@@ -18,8 +18,8 @@
 
 // configuration
 const struct pdm_microphone_config config = {
-  .gpio_data = 2,
-  .gpio_clk = 3,
+  .gpio_data = 22,
+  .gpio_clk = 23,
   .pio = pio0,
   .pio_sm = 0,
   .sample_rate = SAMPLE_RATE,
